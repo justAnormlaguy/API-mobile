c6e43797dbe7f6c3fd7a00268895e2c4c763ae20
justAnormlaguy <lucasl1604@outlook.com>
2026-05-20 21:48:37 -0300

    Implementação do CORS

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="API Biblioteca de Jogos")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"], 
     allow_headers=["*"], 
 )
